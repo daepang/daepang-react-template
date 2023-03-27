14b2f1351ef08fbbe2e948a1db0876df4dfdd300
daepang <dev.daepang@gmail.com>
2023-03-27 15:39:10 +0900

    _app.tsx 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 
+import 'styles/reset.css';
+import 'styles/common.css';
+import 'styles/login.css';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-      <Component {...pageProps} />
-  );
+  return <Component {...pageProps} />;
 }
