5368c84d1ae8ca42261a644dc66a98c55072e474
daepang <dev.daepang@gmail.com>
2023-03-28 15:24:31 +0900

    Login.tsx 구성중

--- src/login/components/templates/Login.tsx
+++ src/login/components/templates/Login.tsx
@@ -7,34 +7,34 @@ const Login = () => {
         <div className={'input_form'}>
           <span className={'inp'} id='loginid-span'>
             <input type='text' placeholder='아이디 입력' title='아이디 입력' />
-            <button type='button'>
-              <span>삭제</span>
+            <button className={'btn_del'} type='button'>
+              <span className={'blind'}>삭제</span>
             </button>
           </span>
-          <p style={{ display: 'none' }}></p>
         </div>
-        <div>
-          <span>
+        <div className={'input_form'}>
+          <span className={'inp'}>
             <input type='password' placeholder='비밀번호 입력 (영문, 숫자, 특수문자 조합)' title='비밀번호 입력' />
-            <button type='button' style={{ display: 'none' }}>
-              <span>삭제</span>
+            <button className={'btn_del'} type='button'>
+              <span className={'blind'}>삭제</span>
             </button>
           </span>
-          <p style={{ display: 'none' }}></p>
         </div>
         <div>
           <p style={{ display: 'none' }}></p>
         </div>
-        <div>
-          <span>
+        <div className={'btn_submit mt20'}>
+          <span className={'checkboxA'}>
             <input type='checkbox' id={'isSaveId'} title='아이디 저장' />
             <label htmlFor={'isSaveId'}>
               <span className={'checkbox_label'}>아이디 저장</span>
             </label>
           </span>
         </div>
-        <div className={'login_opt'}>
-          <button type='button'>로그인</button>
+        <div className={'login_btn'}>
+          <button className={'btnA btn_blue loginbtn'} type='button'>
+            로그인
+          </button>
         </div>
       </div>
     </>
