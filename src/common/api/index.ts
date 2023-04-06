c88ebe7cd08265619811437873f86198bfe95e87
daepang <dev.daepang@gmail.com>
2023-04-06 13:26:55 +0900

    api module 작업중

--- src/common/api/index.ts
+++ src/common/api/index.ts
@@ -13,29 +13,27 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.defaults.timeout = 2500;
+
+// 요청 인터셉터
 axiosInstance.interceptors.request.use(
   config => {
-    //요청을 보내기 전에 수행할 로직
     return config;
   },
   error => {
-    //요청 에러가 발생했을 때 수행할 로직
-    console.log(error); //디버깅
+    console.log(error);
     return Promise.reject(error);
   },
 );
 
-//응답 인터셉터 추가
+// 응답 인터셉터
 axiosInstance.interceptors.response.use(
   response => {
-    //응답에 대한 로직 작성
     const res = response.data;
     return res;
   },
 
   error => {
-    //응답 에러가 발생했을 때 수행할 로직
-    console.log(error); //디버깅
+    console.log(error);
     return Promise.reject(error);
   },
 );
